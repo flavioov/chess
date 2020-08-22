756fbb58bda517cf0d22c7d1178a9af4fc7da248
olive <oliveira.v653>
2020-08-21 22:45:38 -0300

    added board exception and defensive programing at class board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -14,9 +14,9 @@ public class ChessMatch {
     }
 
     public ChessPiece[][] getPieces() {
-        ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumns()];
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 
-        for (int i = 0; i < board.getRow() ; i++) {
+        for (int i = 0; i < board.getRows() ; i++) {
             for (int j = 0; j < board.getColumns() ; j++) {
                 //Down  casting
                 mat[i][j] = (ChessPiece) board.piece(i, j);
@@ -28,7 +28,7 @@ public class ChessMatch {
 
     private void initialSteup() {
         board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(board, Color.BLACK), new Position(0 , 4));
         board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
     }
 }
